48bc4c29d2f795f7618d73aef0677727bff611da
hjs <1141752341@qq.com>
2018-11-17 18:32:08 +0800

    颜色转换

--- JavaScript/js/color.js
+++ JavaScript/js/color.js
@@ -48,3 +48,28 @@
     };
 })();
 
+/** 
+* hex16 进制颜色转 rgb(rgba)
+* @param hex 例如:'#23ff45' 
+* @param opacity 透明度 
+* @returns {string} 
+*/
+function hexToRgba(hex, opacity) {
+    if (opacity) {
+        return 'rgba(' + parseInt('0x' + hex.slice(1, 3)) + ',' + parseInt('0x' + hex.slice(3, 5)) + ',' + parseInt('0x' + hex.slice(5, 7)) + ',' + opacity + ')';
+    } else {
+        return 'rgb(' + parseInt('0x' + hex.slice(1, 3)) + ',' + parseInt('0x' + hex.slice(3, 5)) + ',' + parseInt('0x' + hex.slice(5, 7)) + ')';
+    }
+}
+/**
+ * rgb 转 16进制 hex
+ * @param {string} color rgb
+ */
+function rgbToHex(color) {
+    var rgb = color.split(',');
+    var r = parseInt(rgb[0].split('(')[1]);
+    var g = parseInt(rgb[1]);
+    var b = parseInt(rgb[2].split(')')[0]);
+    var hex = '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    return hex;
+}
