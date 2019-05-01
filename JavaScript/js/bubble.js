b8509333f21faeeff5055543d041315529b05ff3
hjs <1141752341@qq.com>
2019-05-01 10:53:11 +0800

    canvas 气泡绘画

--- JavaScript/js/bubble.js
+++ JavaScript/js/bubble.js
@@ -13,15 +13,18 @@ function start() {
     let updateList = [];
 
     /** 颜色列表 */
-    let colorList = ['orange', 'purple', '#0ae004', '#00c9d0',];
+    let colorList = ['orange', 'purple', '#0ae004', '#00c9d0', '#fcff00', '#ff006c', '#0036ff'];
 
     /** 气泡数量 */
     let bubble_total = 30;
 
-    // 设置 canvas 尺寸
-    canvas.width = 500;
-    canvas.height = 600;
+    /** 最外层容器尺寸 */
+    let size = utils.getRect(utils.find('.page'));
 
+    // 设置 canvas 尺寸
+    canvas.width = size.width;
+    canvas.height = size.height;
+    
     /**
      * 绘画圆
      * @param {CanvasRenderingContext2D} context CanvasRenderingContext2D 对象
@@ -54,6 +57,8 @@ function start() {
         const node = {
             /** 节点半径 */
             radius: 50,
+            /** 最大半径 */
+            max_radius: 80,
             /** 节点 x 位置 */
             x: 0,
             /** 节点 y 位置 */
@@ -62,6 +67,10 @@ function start() {
             color: 'orange',
             /** 透明 */
             opacity: 0.5,
+            /** 左右移动的范围 */
+            move_range: 100,
+            /** 范围 */
+            range: 80,
             /** 水平方向 */
             direction: Math.floor(2 * Math.random()) > 0 ? 1 : -1, 
             /** 移动速度 */
@@ -75,7 +84,9 @@ function start() {
 
         /** 初始化 node 参数 */
         function initNode() {
-            node.radius = Math.floor(50 * Math.random()) + 20;
+            node.radius = 15;
+            node.opacity = 0.15;
+            node.move_range = node.range;
             node.x = Math.floor((canvas.width - node.radius * 2) * Math.random()) + node.radius;
             node.y = canvas.height + node.radius;
             node.color = colorList[Math.floor(colorList.length * Math.random())];
@@ -90,14 +101,37 @@ function start() {
         function move() {
             node.x += node.direction;
             node.y -= node.speed.vertical;
-            // 判断正负水平移动
+
+            // 透明度
+            if (node.opacity < 0.6) {
+                node.opacity += 0.002 * node.speed.vertical;
+                if (node.opacity >= 0.6) node.opacity = 0.6;
+            }
+
+            // 扩大
+            if (node.radius < node.max_radius) {
+                node.radius += 0.1 * node.speed.vertical;
+                if (node.radius >= node.max_radius) node.radius = node.max_radius;
+            }
+
+            // 左右移动
+            node.move_range -= 1;
+            if (node.move_range == 0) {
+                node.move_range = node.range;
+                node.direction = node.direction > 0 ? -1 : 1;
+            }
+
+            // 判断正负水平移动范围
             if (node.x >= canvas.width - node.radius) {
                 node.direction = -1;
+                node.move_range = node.range;
             }
             if (node.x <= node.radius) {
                 node.direction = 1;
+                node.move_range = node.range;
             }
-            // 判断垂直移动
+
+            // 判断垂直移动范围
             if (node.y <= -node.radius) {
                 initNode();
             }
