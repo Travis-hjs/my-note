8b32faa939607fa1ba9f3248f370e7f9d5a241d5
hjs <bleach>
2019-06-24 10:57:03 +0800

    canvas-气泡 代码整理

--- JavaScript/js/bubble.js
+++ JavaScript/js/bubble.js
@@ -1,161 +1,184 @@
-function start() {
-    /** canvas容器 */
-    const canvas = utils.find('.bubble');
-    /** 圆 */
-    const context = canvas.getContext('2d');
+
+/** 节点 class */
+class NodeModule {
+    /**
+     * 节点组件
+     * @param {HTMLCanvasElement} canvas 父容器 canvas
+     * @param {CanvasRenderingContext2D} context 上下文
+     */
+    constructor(canvas, context) {
+        this.canvas = canvas;
+        this.context = context;
+        this.init();
+    }
+
+    /** 节点半径 */
+    radius = 50;
+    /** 最大半径 */
+    max_radius = 80;
+    /** 节点 x 位置 */
+    x = 0;
+    /** 节点 y 位置 */
+    y = 0;
+    /** 节点颜色 */
+    color = 'orange';
+    /** 透明 */
+    opacity = 0.5;
+    /** 左右移动的范围 */
+    move_range = 100;
+    /** 范围 */
+    range = 80;
+    /** 水平方向 */
+    direction = Math.floor(2 * Math.random()) > 0 ? 1 : -1; 
+    /** 移动速度 */
+    speed = {
+        /** 水平速度 */
+        horizontal: 1,
+        /** 垂直速度 */
+        vertical: 1
+    }
+
+    /** 颜色列表 */
+    colorList = ['orange', 'purple', '#0ae004', '#00c9d0', '#fcff00', '#ff006c', '#0036ff'];
+
+    /** 初始化节点参数 */
+    init() {
+        this.radius = 15;
+        this.opacity = 0.15;
+        this.move_range = this.range;
+        this.x = Math.floor((this.canvas.width - this.radius * 2) * Math.random()) + this.radius;
+        this.y = this.canvas.height + this.radius;
+        this.color = this.colorList[Math.floor(this.colorList.length * Math.random())];
+        this.speed.vertical = 2.2 * Math.random() + 0.8;
+    }
+
+    /** 绘画圆 */
+    draw() {
+        // 路径开始 相当于 <div>
+        this.context.beginPath();
+        // 绘画一个圆
+        this.context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+        // 填充色
+        this.context.fillStyle = this.color;
+        // 设置透明度
+        this.context.globalAlpha = this.opacity;
+        // 填充
+        this.context.fill();
+
+        // 描边色
+        // this.context.strokeStyle = '#ccc';
+        // 填充描边
+        // this.context.stroke();
+
+        // 路径介绍 相当于 </div>
+        this.context.closePath();
+    }
+
+    /** 圆运动 */
+    move() {
+        this.x += this.direction;
+        this.y -= this.speed.vertical;
+
+        // 透明度
+        if (this.opacity < 0.6) {
+            this.opacity += 0.002 * this.speed.vertical;
+            if (this.opacity >= 0.6) this.opacity = 0.6;
+        }
+
+        // 扩大
+        if (this.radius < this.max_radius) {
+            this.radius += 0.1 * this.speed.vertical;
+            if (this.radius >= this.max_radius) this.radius = this.max_radius;
+        }
+
+        // 左右移动
+        this.move_range -= 1;
+        if (this.move_range == 0) {
+            this.move_range = this.range;
+            this.direction = this.direction > 0 ? -1 : 1;
+        }
+
+        // 判断正负水平移动范围
+        if (this.x >= this.canvas.width - this.radius) {
+            this.direction = -1;
+            this.move_range = this.range;
+        }
+        if (this.x <= this.radius) {
+            this.direction = 1;
+            this.move_range = this.range;
+        }
+
+        // 判断垂直移动范围
+        if (this.y <= -this.radius) {
+            this.init();
+        }
+        this.draw();
+    }
+
+    /** 每一帧更新 */
+    update() {
+        this.move();
+    }
+}
+
+class Main {
+    /**
+     * 主函数
+     * @param {HTMLElement} el canvas 输出节点
+     * @param {number} total 气泡总数
+     */
+    constructor(el, total) {
+        if (!el) return console.warn('没有指定输出容器节点');
+        /** canvas容器 */
+        this.canvas = document.createElement('canvas');
+        /** 上下文 */
+        this.context = this.canvas.getContext('2d');
+        /** 最外层容器尺寸 */
+        this.size = el.getBoundingClientRect();
+        /** 气泡数量 */
+        this.bubble_total = total || 30;
+        // 输出 canvas 并初始化
+        el.appendChild(this.canvas);
+        this.init();
+    }
+
+    /** 节点列表 */
+    nodeList = [];
+
     /** 清空绘画 */
-    function removeDraw() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
+    remove() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         // console.log('清空 canvas 所有绘画');
     }
 
-    /** 每一帧更新的函数列表 */
-    let updateList = [];
-
-    /** 颜色列表 */
-    let colorList = ['orange', 'purple', '#0ae004', '#00c9d0', '#fcff00', '#ff006c', '#0036ff'];
-
-    /** 气泡数量 */
-    let bubble_total = 30;
-
-    /** 最外层容器尺寸 */
-    let size = utils.getRect(utils.find('.page'));
-
-    // 设置 canvas 尺寸
-    canvas.width = size.width;
-    canvas.height = size.height;
-    
-    /**
-     * 绘画圆
-     * @param {CanvasRenderingContext2D} context CanvasRenderingContext2D 对象
-     * @param {object} node 圆的节点属性
-     */
-    function drawCircular(context, node) {
-        // 路径开始 相当于 <div>
-        context.beginPath();
-        // 绘画一个圆
-        context.arc(node.x, node.y, node.radius, 0, 2 * Math.PI, false);
-        // 填充色
-        context.fillStyle = node.color;
-        // 设置透明度
-        context.globalAlpha = node.opacity;
-        // 填充
-        context.fill();
-
-        // 描边色
-        // context.strokeStyle = '#ccc';
-        // 填充描边
-        // context.stroke();
-
-        // 路径介绍 相当于 </div>
-        context.closePath();
-    }
-
-    /** 创建气泡 */
-    function createBubble() {
-        /** 创建一个节点属性 */
-        const node = {
-            /** 节点半径 */
-            radius: 50,
-            /** 最大半径 */
-            max_radius: 80,
-            /** 节点 x 位置 */
-            x: 0,
-            /** 节点 y 位置 */
-            y: 0,
-            /** 节点颜色 */
-            color: 'orange',
-            /** 透明 */
-            opacity: 0.5,
-            /** 左右移动的范围 */
-            move_range: 100,
-            /** 范围 */
-            range: 80,
-            /** 水平方向 */
-            direction: Math.floor(2 * Math.random()) > 0 ? 1 : -1, 
-            /** 移动速度 */
-            speed: {
-                /** 水平速度 */
-                horizontal: 1,
-                /** 垂直速度 */
-                vertical: 1
-            }
-        };
-
-        /** 初始化 node 参数 */
-        function initNode() {
-            node.radius = 15;
-            node.opacity = 0.15;
-            node.move_range = node.range;
-            node.x = Math.floor((canvas.width - node.radius * 2) * Math.random()) + node.radius;
-            node.y = canvas.height + node.radius;
-            node.color = colorList[Math.floor(colorList.length * Math.random())];
-            node.speed.vertical = 2.2 * Math.random() + 0.8;
+    /** 初始化 */
+    init() {
+        // 初始化 canvas 尺寸
+        this.canvas.width = this.size.width;
+        this.canvas.height = this.size.height;
+        
+        for (let i = 0; i < this.bubble_total; i++) {
+            /** 节点 */
+            const node = new NodeModule(this.canvas, this.context);
+            this.nodeList.push(node);
         }
-
-        initNode();
-        drawCircular(context, node);
-        // console.log('节点属性', node);
-
-        /** 圆运动 */
-        function move() {
-            node.x += node.direction;
-            node.y -= node.speed.vertical;
-
-            // 透明度
-            if (node.opacity < 0.6) {
-                node.opacity += 0.002 * node.speed.vertical;
-                if (node.opacity >= 0.6) node.opacity = 0.6;
-            }
-
-            // 扩大
-            if (node.radius < node.max_radius) {
-                node.radius += 0.1 * node.speed.vertical;
-                if (node.radius >= node.max_radius) node.radius = node.max_radius;
-            }
-
-            // 左右移动
-            node.move_range -= 1;
-            if (node.move_range == 0) {
-                node.move_range = node.range;
-                node.direction = node.direction > 0 ? -1 : 1;
-            }
-
-            // 判断正负水平移动范围
-            if (node.x >= canvas.width - node.radius) {
-                node.direction = -1;
-                node.move_range = node.range;
-            }
-            if (node.x <= node.radius) {
-                node.direction = 1;
-                node.move_range = node.range;
-            }
-
-            // 判断垂直移动范围
-            if (node.y <= -node.radius) {
-                initNode();
-            }
-            drawCircular(context, node);
-        }
-        // 添加到动画帧更新
-        updateList.push(move);
-    }
-
-    for (let i = 0; i < bubble_total; i++) {
-        createBubble();
-    }
-
-    utils.update(function () {
-        if (updateList.length) {
-            // 这里一定要清空再重新绘画
-            removeDraw();
-            for (let i = 0; i < updateList.length; i++) {
-                const item = updateList[i];
-                if (typeof item === 'function') {
-                    item();
+        
+        utils.update(() => {
+            if (this.nodeList.length > 0) {
+                // 这里一定要清空再重新绘画
+                this.remove();
+                for (let i = 0; i < this.nodeList.length; i++) {
+                    const node = this.nodeList[i];                    
+                    if (node instanceof NodeModule) {
+                        node.update();
+                    }
                 }
             }
-        }
-    });
+        });
+    }
 }
-start();
+
+let page = utils.find('.page');
+
+new Main(page);
+
