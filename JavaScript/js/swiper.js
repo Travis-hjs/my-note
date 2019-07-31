15b15822911217cf0bfe9fb3f8f048e15647e099
hjs <bleach>
2019-07-31 18:36:48 +0800

    swiper 注释修改

--- JavaScript/js/swiper.js
+++ JavaScript/js/swiper.js
@@ -13,7 +13,7 @@ function swiper(params) {
     /** 是否需要底部圆点 */
     let pagination = false; 
     /** 是否需要回路 */
-    let loop = false;
+    let isLoop = false;
     /** 方向 true = X & Y = false */
     let direction = false; 
     /** 是否需要自动播放 */
@@ -22,9 +22,11 @@ function swiper(params) {
     let interval = 3000; 
     /** 过渡时间（毫秒）默认 300 */
     let moveTime = 300;
-
-    /** css class 命名列表 */
-    let classNames = ['.swiper_list', '.swiper_item', '.swiper_dot', '.swiper_dot_active', '.swiper_pagination'];
+    /**
+     * css class 命名列表
+     * @type {['滑动列表','滑动item','底部圆点','圆点高亮','圆点容器']}
+     */
+    const classNames = ['.swiper_list', '.swiper_item', '.swiper_dot', '.swiper_dot_active', '.swiper_pagination'];
 
     /**
      * 触摸事件
@@ -40,27 +42,30 @@ function swiper(params) {
         /** 底部圆点 */
         let dots = div.querySelectorAll(classNames[2]);
         /** 触摸开始时间 */
-        let start_time = 0; 
+        let startTime = 0; 
         /** 触摸结束时间 */
-        let end_time = 0;
+        let endTime = 0;
         /** 开始的距离 */
-        let start_distance = 0;
+        let startDistance = 0;
         /** 结束的距离 */ 
-        let end_distance = 0;
+        let endDistance = 0;
         /** 结束距离状态 */
-        let end_state = 0;
+        let endState = 0;
         /** 移动的距离 */
-        let move_distance = 0;
-        /** 圆点位置 当前 item 索引 */
+        let moveDistance = 0;
+        /** 圆点位置 && 当前 item 索引 */
         let index = 0;
-        /** 自动播放定计算数值 */
+        /** 动画帧计数 */
         let count = 0;
-        /** loop定时器计算值 */
-        let loop_num = 0;
-        /** 距离 */
-        let distance = direction ? height : width;
-        /** 定义 requestAnimationFrame */
-        let myAnimation = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+        /** loop 帧计数 */
+        let loopCount = 0;
+        /** 移动范围 */
+        let range = direction ? height : width;
+        /** 
+         * 动画帧 
+         * @type {requestAnimationFrame}
+         */
+        let animationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
         
         /** 设置动画 */
         function startAnimation() {
@@ -91,21 +96,21 @@ function swiper(params) {
             /** 拖动距离 */
             let _d = 0;
             // 默认这个公式
-            _d = move_distance + (end_distance - start_distance);
+            _d = moveDistance + (endDistance - startDistance);
             // 判断最大正负值
-            if ((end_distance - start_distance) >= distance) {
-                _d = move_distance + distance;
-            } else if ((end_distance - start_distance) <= -distance) {
-                _d = move_distance - distance;
+            if ((endDistance - startDistance) >= range) {
+                _d = moveDistance + range;
+            } else if ((endDistance - startDistance) <= -range) {
+                _d = moveDistance - range;
             }
-            // 没有loop的时候惯性拖拽
-            if (!loop) {
-                if ((end_distance - start_distance) > 0 && index === 0) {
+            // 没有 loop 的时候惯性拖拽
+            if (!isLoop) {
+                if ((endDistance - startDistance) > 0 && index === 0) {
                     // console.log('到达最初');
-                    _d = move_distance + ((end_distance - start_distance) - ((end_distance - start_distance) * 0.6));
-                } else if ((end_distance - start_distance) < 0 && index === items.length - 1) {
+                    _d = moveDistance + ((endDistance - startDistance) - ((endDistance - startDistance) * 0.6));
+                } else if ((endDistance - startDistance) < 0 && index === items.length - 1) {
                     // console.log('到达最后');
-                    _d = move_distance + ((end_distance - start_distance) - ((end_distance - start_distance) * 0.6));
+                    _d = moveDistance + ((endDistance - startDistance) - ((endDistance - startDistance) * 0.6));
                 }
             }
             return _d;
@@ -117,13 +122,13 @@ function swiper(params) {
          */
         function judgeTouch(_d) {
             //	这里我设置了200毫秒的有效拖拽间隔
-            if ((end_time - start_time) < 200) return true;
+            if ((endTime - startTime) < 200) return true;
             // 这里判断方向（正值和负值）
             if (_d < 0) {
-                if ((end_distance - start_distance) < (_d / 2)) return true;
+                if ((endDistance - startDistance) < (_d / 2)) return true;
                 return false;
             } else {
-                if ((end_distance - start_distance) > (_d / 2)) return true;
+                if ((endDistance - startDistance) > (_d / 2)) return true;
                 return false;
             }
         }
@@ -131,7 +136,7 @@ function swiper(params) {
         /** 返回原来位置 */
         function backLocation() {
             startAnimation();
-            slideStyle(move_distance);
+            slideStyle(moveDistance);
         }
         
         /**
@@ -141,27 +146,27 @@ function swiper(params) {
         function slideMove(_d) {
             startAnimation();
             slideStyle(_d);
-            loop_num = 0;
-            // 判断loop时回到第一张或最后一张
-            if (loop && index < 0) {
+            loopCount = 0;
+            // 判断 loop 时回到第一张或最后一张
+            if (isLoop && index < 0) {
                 // 我这里是想让滑块过渡完之后再重置位置所以加的延迟 (之前用setTimeout，快速滑动有问题，然后换成 requestAnimationFrame解决了这类问题)
                 function loopMoveMin() {
-                    loop_num += 1;
-                    if (loop_num < moveTime / 1000 * 60) return myAnimation(loopMoveMin);
+                    loopCount += 1;
+                    if (loopCount < moveTime / 1000 * 60) return animationFrame(loopMoveMin);
                     stopAnimation();
-                    slideStyle(distance * -(items.length - 3));
+                    slideStyle(range * -(items.length - 3));
                     // 重置一下位置
-                    move_distance = distance * -(items.length - 3);
+                    moveDistance = range * -(items.length - 3);
                 }
                 loopMoveMin();
                 index = items.length - 3;
-            } else if (loop && index > items.length - 3) {
+            } else if (isLoop && index > items.length - 3) {
                 function loopMoveMax() {
-                    loop_num += 1;
-                    if (loop_num < moveTime / 1000 * 60) return myAnimation(loopMoveMax);
+                    loopCount += 1;
+                    if (loopCount < moveTime / 1000 * 60) return animationFrame(loopMoveMax);
                     stopAnimation();
                     slideStyle(0);
-                    move_distance = 0;
+                    moveDistance = 0;
                 }
                 loopMoveMax();
                 index = 0;
@@ -176,42 +181,42 @@ function swiper(params) {
         /** 判断移动 */ 
         function judgeMove() {
             // 判断是否需要执行过渡
-            if (end_distance < start_distance) {
+            if (endDistance < startDistance) {
                 // 往上滑动 or 向左滑动
-                if (judgeTouch(-distance)) {
+                if (judgeTouch(-range)) {
                     // 判断有loop的时候不需要执行下面的事件
-                    if (!loop && move_distance === (-(items.length - 1) * distance)) return backLocation();
+                    if (!isLoop && moveDistance === (-(items.length - 1) * range)) return backLocation();
                     index += 1;
-                    slideMove(move_distance - distance);
-                    move_distance -= distance;
+                    slideMove(moveDistance - range);
+                    moveDistance -= range;
                 } else backLocation();
             } else {
                 // 往下滑动 or 向右滑动
-                if (judgeTouch(distance)) {
-                    if (!loop && move_distance === 0) return backLocation();
+                if (judgeTouch(range)) {
+                    if (!isLoop && moveDistance === 0) return backLocation();
                     index -= 1;
-                    slideMove(move_distance + distance);
-                    move_distance += distance;
+                    slideMove(moveDistance + range);
+                    moveDistance += range;
                 } else backLocation();
             }
         }
         
         /** 自动播放移动 */
         function autoMove() {
-            // 这里判断是否有回路loop的自动播放
-            if (loop) {
+            // 这里判断 loop 的自动播放
+            if (isLoop) {
                 index += 1;
-                slideMove(move_distance - distance);
-                move_distance -= distance;
+                slideMove(moveDistance - range);
+                moveDistance -= range;
             } else {
                 if (index >= items.length - 1) {
                     index = 0;
                     slideMove(0);
-                    move_distance = 0;
+                    moveDistance = 0;
                 } else {
                     index += 1;
-                    slideMove(move_distance - distance);
-                    move_distance -= distance;
+                    slideMove(moveDistance - range);
+                    moveDistance -= range;
                 }
             }
         }
@@ -219,8 +224,7 @@ function swiper(params) {
         /** 开始自动播放 */
         function startAuto() {
             count += 1;
-            //	这里帧数是1秒60次
-            if (count < interval / 1000 * 60) return myAnimation(startAuto);
+            if (count < interval / 1000 * 60) return animationFrame(startAuto);
             count = 0;
             autoMove();
             startAuto();
@@ -231,33 +235,35 @@ function swiper(params) {
 
         // 开始触摸
         list.addEventListener('touchstart', ev => {
-            // loop_num = moveTime/1000*60;
-            [start_time, count, loop_num] = [new Date().getTime(), 0, moveTime / 1000 * 60];
+            startTime = new Date().getTime();
+            count = 0;
+            loopCount = moveTime / 1000 * 60;
             stopAnimation();
-            start_distance = direction ? ev.touches[0].pageY : ev.touches[0].pageX;
+            startDistance = direction ? ev.touches[0].pageY : ev.touches[0].pageX;
         });
 
         // 触摸移动
         list.addEventListener('touchmove', ev => {
             ev.preventDefault();
             count = 0;
-            end_distance = direction ? ev.touches[0].pageY : ev.touches[0].pageX;
+            endDistance = direction ? ev.touches[0].pageY : ev.touches[0].pageX;
             slideStyle(touchRange());
         });
 
         // 触摸离开
         list.addEventListener('touchend', () => {
-            end_time = new Date().getTime();
+            endTime = new Date().getTime();
             // 判断是否点击
-            if (end_state !== end_distance) {
+            if (endState !== endDistance) {
                 judgeMove();
             } else {
-                // console.log('执行');
                 backLocation();
             }
             // console.log(`index: ${index}`);	//  这里可以做触摸之后位置回调
-            // 更新位置 && 重新打开自动播放要放到最后
-            [end_state, count] = [end_distance, 0];
+            // 更新位置 
+            endState = endDistance;
+            // 重新打开自动播
+            count = 0;
         });
     }
     
@@ -329,7 +335,7 @@ function swiper(params) {
         let moveWidth = node.offsetWidth, 
             moveHeight = node.offsetHeight;
         if (pagination) outputPagination(node);
-        if (loop) {
+        if (isLoop) {
             outputLoop(node, moveWidth, moveHeight);
         } else {
             layout(node, moveWidth, moveHeight);
@@ -343,7 +349,7 @@ function swiper(params) {
         pagination = params.pagination || false;
         direction = params.vertical || false;
         autoPaly = params.autoPaly || false;
-        loop = params.loop || false;
+        isLoop = params.loop || false;
         moveTime = params.moveTime || 300;
         interval = params.interval || 3000;
         format(params.el);
