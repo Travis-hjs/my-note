daae2f19e85db9d6900d4b58409e6418853b377c
hjs <bleach>
2019-10-10 11:07:59 +0800

    下拉刷新代码整理

--- JavaScript/js/dropDownRefresh.js
+++ JavaScript/js/dropDownRefresh.js
@@ -7,6 +7,7 @@
  * @param {object} option 配置
  * @param {HTMLElement} option.el 下拉元素（必选）
  * @param {number} option.distance 下拉距离[px]（可选）
+ * @param {number} option.deviation 顶部往下偏移量[px]（可选）
  * @param {string} option.loadIcon 下拉中的 icon html（可选）
  */
 function dropDownRefresh(option) {
@@ -15,13 +16,15 @@ function dropDownRefresh(option) {
     const page = option.el;
     /** 下拉距离 */
     const distance = option.distance || 88;
+    /** 顶部往下偏移量 */
+    const deviation = option.deviation || 0;
     /** 顶层节点 */
     const topNode = doc.createElement('div');
     /** 下拉时遮罩 */
     const maskNode = doc.createElement('div');
 
     topNode.innerHTML = `<div refresh-icon style="transition: .2s all;"><svg style="transform: rotate(90deg); display: block;" t="1570593064555" viewBox="0 0 1575 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="26089" width="48" height="48"><path d="M1013.76 0v339.968H484.115692V679.778462h529.644308v339.968l529.644308-485.612308v-48.600616L1013.76 0zM243.396923 679.857231h144.462769V339.968H243.396923V679.778462z m-240.797538 0h144.462769V339.968H2.599385V679.778462z" fill="#000000" fill-opacity=".203" p-id="26090"></path></svg></div><div refresh-loading style="display: none; animation: refresh-loading 1s linear infinite;">${option.loadIcon || '<p style="font-size: 15px; color: #666;">loading...</p>'}</div>`;
-    topNode.style.cssText = `width: 100%; height: ${distance}px; position: fixed; top: ${-distance}px; left: 0; z-index: 10; display: flex; flex-wrap: wrap; align-items: center; justify-content: center; box-sizing: border-box; margin: 0; padding: 0;`;
+    topNode.style.cssText = `width: 100%; height: ${distance}px; position: fixed; top: ${-distance + deviation}px; left: 0; z-index: 10; display: flex; flex-wrap: wrap; align-items: center; justify-content: center; box-sizing: border-box; margin: 0; padding: 0;`;
     maskNode.style.cssText = 'position: fixed; top: 0; left: 0; width: 100%; height: 100vh; box-sizing: border-box; margin: 0; padding: 0; background-color: rgba(0,0,0,0); z-index: 999;';
     page.parentNode.insertBefore(topNode, page);
 
@@ -65,6 +68,7 @@ function dropDownRefresh(option) {
             // 触摸开始
             page.addEventListener('touchstart', function (e) {
                 startDistance = e.touches[0].pageY;
+                scrollTop = 1;
                 setAnimation(0);
             });
 
@@ -91,13 +95,13 @@ function dropDownRefresh(option) {
                     // 回调距离函数 如果有需要
                     if (typeof rangeCallback === 'function') rangeCallback(range);
                 }
-            });
+            }, false);
 
             // 触摸结束
             page.addEventListener('touchend', function () {
                 setAnimation(0.3);
                 // console.log(`移动的距离：${range}, 最大距离：${distance}`);
-                if (range > distance && range > 1 && scrollTop == 0) {
+                if (range > distance && range > 1 && scrollTop === 0) {
                     setSlide(distance);
                     doc.body.appendChild(maskNode);
                     // 阻止往上滑动
