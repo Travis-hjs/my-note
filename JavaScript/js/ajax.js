0ea859970c6523b181f51f319ba668095bdd1e9a
fengxin19860616 <56420139+fengxin19860616@users.noreply.github.com>
2019-10-18 18:19:26 +0800

    http代码整理

--- JavaScript/js/ajax.js
+++ JavaScript/js/ajax.js
@@ -2,44 +2,46 @@
 const BASEURL = 'http://che.qihao.lzei.com';
 
 /**
- * fetch 请求 
+ * `fetch`请求 
  * @dec learn：https://developer.mozilla.org/zh-CN/docs/Web/API/Fetch_API
- * @param {'GET'|'POST'} method 请求方法 => 这里我只枚举了常用的两种
+ * @param {'GET'|'POST'} method 请求方法：这里我只枚举了常用的两种
  * @param {string} url 请求路径
  * @param {object} data 请求参数对象
  */
-function fetchRequest(method, url, data) {
-    /** 请求选项设置 */
-    let options = {
-        // credentials: 'include', // 打开 cookie
-        // mode: 'cors',           // 打开跨域（需要后台设置）
-        method: method,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: null
-    };
+function theFetch(method, url, data = {}) {
+    let dataPost = null;
+    let dataGet = '';
     switch (method) {
         case 'POST':
             // 若后台没设置接收 JSON 则不行 需要跟 GET 一样的解析对象传参
-            options.body = JSON.stringify(data);
+            dataPost = JSON.stringify(data);
             break;
 
         case 'GET':
-            // 判断是否一个空对象
-            if (JSON.stringify(data) != '{}') {
-                /** 参数拼接字符串 */
-                let str = '';
-                // 解析对象传参
-                for (let key in data) str += '&' + key + '=' + data[key];
-                if (str) str = '?' + str.slice(1);
-                url += str;
+            // 解析对象传参
+            for (const key in data) {
+                dataGet += `&${key}=${data[key]}`;
+            }
+            if (dataGet) {
+                dataGet = '?' + dataGet.slice(1);
             }
             break;
     }
-    return new Promise(function (resolve, reject) {
-        fetch(url, options).then(response => {
-            // 请求状态 => 注意，这里不转换 json 下面会 undefined
+    return new Promise((resolve, reject) => {
+        fetch(url + dataGet, {
+            // credentials: 'include',  // 携带cookie配合后台用
+            // mode: 'cors',            // 貌似也是配合后台设置用的跨域模式
+            method: method,
+            headers: {
+                /**  
+                 * 通常为`application/json`因为等下我要请求豆瓣的接口
+                 * 所以这里用`application/x-www-form-urlencoded`
+                */
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            body: dataPost
+        }).then(response => {
+            // 把响应的信息转为`json`
             return response.json();
         }).then(res => {
             resolve(res);
@@ -50,16 +52,10 @@ function fetchRequest(method, url, data) {
 }
 
 function fetchData() {
-    fetchRequest('post', BASEURL + '/api/app/parking', {
-        appkey: 'e2fb20ea3f3df33310788a4247834c93',
-        token: '2a11d6d67a8b8196afbcefbac3e0a573',
-        page: '1',
-        limit: '7',
-        longitude: '113.30764968',
-        latitude: '23.1200491',
-        sort: 'distance',
-        order: 'asc',
-        keyword: ''
+    // 这里是豆瓣的接口
+    theFetch('GET', 'https://douban.uieee.com/v2/movie/top250', {
+        start: 1,
+        count: 5
     }).then(res => {
         console.log('Fetch success', res);
     }).catch(err => {
@@ -72,7 +68,7 @@ function fetchData() {
  * @dec learn: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
  * @param {object} param 传参对象
  * @param {string} param.url 请求路径
- * @param {'GET'|'POST'} param.method 请求方法 => 这里我只枚举了常用的两种
+ * @param {'GET'|'POST'} param.method 请求方法：这里我只枚举了常用的两种
  * @param {object} param.data 传参对象
  * @param {FormData} param.file 上传图片`FormData`
  * @param {number} param.overtime 超时检测毫秒数
