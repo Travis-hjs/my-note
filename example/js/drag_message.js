d278efcb83522fc7233b23cd02d5e8e1b353e186
1141752341@qq.com <z1141752341>
2020-05-16 15:46:27 +0800

    note

--- example/js/drag_message.js
+++ example/js/drag_message.js
@@ -5,6 +5,127 @@
      * @returns {HTMLElement} 
      */
     const find = el => document.querySelector(el);
+    
+    /** 聊天内容（类型提示用） */
+    const messageInfo = {
+        /** 是否为自己 */
+        self: false,
+        /** 聊天内容 */
+        content: '',
+        /** 聊天时间 */
+        date: ''
+    }
+
+    /** 用户数据（类型提示用） */
+    const userInfo = {
+        /** 用户id */
+        id: 0, 
+        /** 用户名 */
+        name: '', 
+        /** 用户头像 */
+        image: '',
+        /** 个性签名 */
+        label: '',
+        /** 
+         * 聊天记录
+         * @type {Array<messageInfo>}
+         * 
+        */
+        messages: []
+    }
+
+    /**
+     * 用户数据列表
+     * @type {Array<userInfo>}
+     */
+    const userData = [
+        {
+            name: '刀锋之影-泰隆',
+            image: 'https://game.gtimg.cn/images/lol/act/img/champion/Talon.png',
+            label: '刀下生，刀下死'
+        }, {
+            name: '德玛西亚之翼-奎因',
+            image: 'https://game.gtimg.cn/images/lol/act/img/champion/Quinn.png',
+            label: '遮住ta的眼睛！'
+        }, {
+            name: '猩红收割者-弗拉基米尔',
+            image: 'https://game.gtimg.cn/images/lol/act/img/champion/Vladimir.png',
+            label: '嘶~呼~'
+        }, {
+            name: '琴瑟仙女-娑娜',
+            image: 'https://game.gtimg.cn/images/lol/act/img/champion/Sona.png',
+            label: '奏乐模式开始了'
+        }, {
+            name: '影流之主-劫',
+            image: 'https://game.gtimg.cn/images/lol/act/img/champion/Zed.png',
+            label: '无知者，在劫难逃'
+        }, {
+            name: '赏金猎人-厄运小姐',
+            image: 'https://game.gtimg.cn/images/lol/act/img/champion/MissFortune.png',
+            label: '好运，不会眷顾傻瓜'
+        }, {
+            name: '光辉女郎-拉克丝',
+            image: 'https://game.gtimg.cn/images/lol/act/img/champion/Lux.png',
+            label: '照亮你的未来'
+        }
+    ];
+
+    /** 自己头像 */
+    const selfHead = 'https://mirror-gold-cdn.xitu.io/16b64a3a3b2baec66af?imageView2/1/w/100/h/100/q/85/format/webp/interlace/1';
+
+    /** 聊天窗口 */
+    const messageWindow = find('.message_window');
+    /** 聊天界面联系人列表 */
+    const messageList = find('.message_window .user_list');
+    /** 聊天记录容器 */
+    const messageContent = find('.message_window .content');
+    /** 聊天联系人节点 */
+    const contactBox = find('.contact_box');
+    /** 聊天联系人列表节点 */
+    const contactList = find('.contact_box .contact');
+    /** 打开`聊天联系人列表`节点 */
+    const openContactBox = find('.open_contact_box');
+    /** 
+     * 输入框节点
+     * @type {HTMLTextAreaElement} 
+     * 
+    */
+    const inputBox = find('.message_window .input_box');
+    /** 当前聊天用户节点对象 */
+    const currentMessageUser = {
+        /**
+         * @type {HTMLImageElement}
+         */
+        head: find('.content_box .top .user_head'),
+        name: find('.content_box .top .user_name'),
+        label: find('.content_box .user_label')
+    }
+
+    /** 当前聊天用户`id`在列表中的索引 */
+    let currentMessageIndex = 0;
+    /** 输入框输入内容 */
+    let inputValue = '';
+    /** 自动回复计时器 */
+    let autoReplyTimer = 0;
+
+    /**
+     * 随机生成中文
+     * @param {number} min 
+     * @param {number} max 
+     */
+    function randomText(min, max) {
+        const len = parseInt(Math.random() * max) + min;
+        const base = 20000;
+        const range = 1000;
+        let str = '';
+        let i = 0;
+        while (i < len) {
+            i++;
+            const lower = parseInt(Math.random() * range);
+            str += String.fromCharCode(base + lower);
+        }
+        return str;
+    }
 
     /**
      * 初始化鼠标事件
@@ -64,7 +185,7 @@
 
     /**
      * 输出聊天界面列表节点
-     * @param {Array<{id: number, name: string, image: string, label: string}>} list 
+     * @param {Array<userInfo>} list 
      */
     function outputUserList(list) {
         for (let i = 0; i < list.length; i++) {
@@ -74,7 +195,7 @@
             el.className = 'user_item flex fvertical';
             el.innerHTML = `<img class="user_head" src="${item.image}" alt=""><div class="user_name f1">${item.name}</div>`;
             el.addEventListener('click', function() {
-                switchUserItemState(item.id, item);
+                switchUserItemState(item.id);
                 setCurrentUserInfo(item);
             });
             messageList.appendChild(el);
@@ -83,7 +204,7 @@
 
     /**
      * 输出联系人列表节点
-     * @param {Array<{id: number, name: string, image: string, label: string}>} list 
+     * @param {Array<userInfo>} list 
      */
     function outputContactList(list) {
         for (let i = 0; i < list.length; i++) {
@@ -99,7 +220,7 @@
             el.addEventListener('click', function() {
                 messageWindow.classList.remove('message_window_hide');
                 closeMessageList();
-                switchUserItemState(item.id, item);
+                switchUserItemState(item.id);
                 setCurrentUserInfo(item);
             });
             contactList.appendChild(el);
@@ -109,9 +230,8 @@
     /**
      * 切换聊天界面联系人状态
      * @param {number} id 
-     * @param {{id: number, name: string, image: string, label: string}} info
      */
-    function switchUserItemState(id, info) {
+    function switchUserItemState(id) {
         const className = 'user_item_active';
         for (let i = 0; i < messageList.children.length; i++) {
             const item = messageList.children[i];
@@ -125,13 +245,103 @@
 
     /**
      * 设置当前用户信息
-     * @param {{id: number, name: string, image: string, label: string}} info 
+     * @param {userInfo} info 
      */
     function setCurrentUserInfo(info) {
-        // console.log(info);
+        // console.log('设置当前用户信息', info);
         currentMessageUser.head.src = info.image;
         currentMessageUser.name.textContent = info.name;
         currentMessageUser.label.textContent = info.label;
+        for (let i = 0; i < userData.length; i++) {
+            const item = userData[i];
+            if (item.id == info.id) {
+                currentMessageIndex = i;
+                break;
+            }
+        }
+        updateRecord();
+    }
+
+    /** 更新聊天记录 */
+    function updateRecord() {
+        messageContent.innerHTML = '';
+        for (let i = 0; i < userData[currentMessageIndex].messages.length; i++) {
+            const record = userData[currentMessageIndex].messages[i];
+            outputRecordItem(record);
+        }
+        messageContentToBottom(true);
+    }
+
+    /**
+     * 输出单条聊天记录
+     * @param {messageInfo} data 
+     */
+    function outputRecordItem(data) {
+        const el = document.createElement('div');
+        if (data.self) {
+            el.className = 'message flex message_right';
+            el.innerHTML = `<div class="placeholder"></div>
+            <div class="f1">
+                <div class="state">${data.date}</div>
+                <div class="text">${data.content}</div>
+            </div>
+            <img class="user_head" src="${selfHead}" alt="">`
+        } else {
+            el.className = 'message flex message_left';
+            el.innerHTML = `<img class="user_head" src="${userData[currentMessageIndex].image}" alt="">
+            <div class="f1">
+                <div class="state">${data.date}</div>
+                <div class="text">${data.content}</div>
+            </div>
+            <div class="placeholder"></div>`
+        }
+        messageContent.appendChild(el);
+    }
+
+    /** 自己发送一条信息 */
+    function sendSelfMessage() {
+        if (inputValue.trim() == '') return alert('输入的内容不能为空');
+        const data = {
+            self: true,
+            content: inputValue.trim(),
+            date: new Date().toLocaleString()
+        }
+        userData[currentMessageIndex].messages.push(data);
+        inputValue = '';
+        inputBox.value = '';
+        outputRecordItem(data);
+        messageContentToBottom();
+        clearTimeout(autoReplyTimer);
+        autoReplyTimer = setTimeout(sendAutoMessage, 1000);
+    }
+
+    /** 机器人自动回复信息 */
+    function sendAutoMessage() {
+        const value = randomText(2, 30);
+        const data = {
+            self: false,
+            content: '【自动回复】' + value,
+            date: new Date().toLocaleString()
+        }
+        userData[currentMessageIndex].messages.push(data);
+        outputRecordItem(data);
+        messageContentToBottom();
+    }
+
+    /** 
+     * 将聊天界面容器滚动至底 
+     * @param {boolean} init 是否初始化
+    */
+    function messageContentToBottom(init) {
+        let height = 0;
+        for (let i = 0; i < messageContent.children.length; i++) {
+            const item = messageContent.children[i];
+            height += item.clientHeight;
+        }
+        messageContent.scrollTo({
+            top: height,
+            behavior: init ? 'auto' : 'smooth'
+        });
     }
 
     /** 关闭联系人列表 */
@@ -139,65 +349,10 @@
         openContactBox.classList.remove('hide');
         contactBox.classList.add('contact_box_hide');
     }
-    
-    /** 聊天窗口 */
-    const messageWindow = find('.message_window');
-    /** 聊天界面联系人列表 */
-    const messageList = find('.message_window .user_list');
-    /** 聊天联系人节点 */
-    const contactBox = find('.contact_box');
-    /** 聊天联系人列表节点 */
-    const contactList = find('.contact_box .contact');
-    /** 打开`聊天联系人列表`节点 */
-    const openContactBox = find('.open_contact_box');
-    /** 当前聊天用户节点对象 */
-    const currentMessageUser = {
-        /**
-         * @type {HTMLImageElement}
-         */
-        head: find('.content_box .top .user_head'),
-        name: find('.content_box .top .user_name'),
-        label: find('.content_box .user_label')
-    }
-
-    /**
-     * 用户数据
-     * @type {Array<{id: number, name: string, image: string, label: string}>}
-     */
-    const userData = [
-        {
-            name: '刀锋之影-泰隆',
-            image: 'https://game.gtimg.cn/images/lol/act/img/champion/Talon.png',
-            label: '刀下生，刀下死'
-        }, {
-            name: '德玛西亚之翼-奎因',
-            image: 'https://game.gtimg.cn/images/lol/act/img/champion/Quinn.png',
-            label: '遮住ta的眼睛！'
-        }, {
-            name: '猩红收割者-弗拉基米尔',
-            image: 'https://game.gtimg.cn/images/lol/act/img/champion/Vladimir.png',
-            label: '嘶~呼~'
-        }, {
-            name: '琴瑟仙女-娑娜',
-            image: 'https://game.gtimg.cn/images/lol/act/img/champion/Sona.png',
-            label: '奏乐模式开始了'
-        }, {
-            name: '影流之主-劫',
-            image: 'https://game.gtimg.cn/images/lol/act/img/champion/Zed.png',
-            label: '无知者，在劫难逃'
-        }, {
-            name: '赏金猎人-厄运小姐',
-            image: 'https://game.gtimg.cn/images/lol/act/img/champion/MissFortune.png',
-            label: '好运，不会眷顾傻瓜'
-        }, {
-            name: '光辉女郎-拉克丝',
-            image: 'https://game.gtimg.cn/images/lol/act/img/champion/Lux.png',
-            label: '照亮你的未来'
-        }
-    ];
 
     userData.forEach((item, index) => {
-        item.id = index;
+        item.id = index * 2;
+        item.messages = [];
     });
 
     outputUserList(userData);
@@ -219,6 +374,20 @@
     messageWindow.querySelector('.close_window').addEventListener('click', function() {
         messageWindow.classList.add('message_window_hide');
     });
+
+    // 监听输入框输入事件
+    inputBox.addEventListener('keydown', function(event) {
+        // console.log(this.value, event);
+        if (event.keyCode == 13) {
+            sendSelfMessage();
+        }
+    });
+
+    inputBox.addEventListener('input', function() {
+        inputValue = this.value;
+    });
+
+    find('.message_window .send_btn').addEventListener('click', sendSelfMessage);
 
     // https://lol.qq.com/data/info-heros.shtml
     
