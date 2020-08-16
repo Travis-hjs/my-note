f71bfc1ce6792d21360bd7723b4a2155d01d79a2
1141752341@qq.com <z1141752341>
2020-08-16 19:09:34 +0800

    js

--- JavaScript/js/utils.js
+++ JavaScript/js/utils.js
@@ -36,8 +36,8 @@ class ModuleString {
     filterSpecialValue(value) {
         value = value.trim();
         const emojiReg = /[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF][\u200D|\uFE0F]|[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF]|[0-9|*|#]\uFE0F\u20E3|[0-9|#]\u20E3|[\u203C-\u3299]\uFE0F\u200D|[\u203C-\u3299]\uFE0F|[\u2122-\u2B55]|\u303D|[\A9|\AE]\u3030|\uA9|\uAE|\u3030/ig;
-        const reg = /[`~!@#$^&()=|{}':;,\[\].<>/?！￥…*（）—【】‘；：”“。，、？%+\-_"\\]/g;
-        return value.replace(emojiReg, "").replace(reg, "");
+        const symbolReg = /[`~!@#$^&()=|{}':;,\[\].<>/?！￥…*（）—【】‘；：”“。，、？%+\-_"\\]/g; // 漏了可以自己加进去`
+        return value.replace(emojiReg, "").replace(symbolReg, "");
     }
 
     /**
