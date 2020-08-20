c4ff7dab9760b828dc922f1e5db6635fa3b756e0
1141752341@qq.com <z1141752341>
2020-08-20 20:43:03 +0800

    canvas 圆环代码优化

--- JavaScript/js/canvas_ring.js
+++ JavaScript/js/canvas_ring.js
@@ -2,18 +2,18 @@
 /**
  * 百分百圆环
  * @param {object} info 圆环的信息
- * @param {HTMLCanvasElement} info.el 承载的节点
- * @param {number} info.width 宽
- * @param {number} info.height 高
+ * @param {HTMLElement} info.el 挂载的节点
  * @param {number} info.lineWidth 圆环的宽度 
- * @param {string} info.lineColor 圆环的颜色: rgba()|rgb()|#xxx|orange
- * @param {string} info.lineLayerColor 圆环的底色: rgba()|rgb()|#xxx|orange
- * @param {string} info.backgroundColor 圆环的底色: rgba()|rgb()|#xxx|orange
- * @param {string} info.fontStyle 字体样式："bold 35px Arial"
+ * @param {string} info.lineColor 圆环的颜色: rgba() | rgb() | #xxx | orange
+ * @param {string} info.lineLayerColor 圆环的底色: rgba() | rgb() | #xxx | orange
+ * @param {string} info.backgroundColor 背景颜色: rgba() | rgb() | #xxx | orange
  * @param {string} info.fontColor 字体颜色：rgba()|rgb()|#xxx|orange
+ * @param {number} info.fontSize 字体大小（像素）
+ * @param {"bold"} info.fontWeight 字重：只有"bold"和不传（普通）两种
+ * @param {"Arial"|"Helvetica"} info.fontStyle 字体样式，只枚举了两种，具体跟`css`一致
  */
 function ringProgress(info) {
-    if (!info.el) return console.warn("没有设置 canvas 节点!!!");
+    if (!info.el) return console.warn("没有设置挂载 canvas 的节点!!!");
     /**
      * 动画帧
      * @type {requestAnimationFrame}
@@ -24,31 +24,35 @@ function ringProgress(info) {
      * @type {cancelAnimationFrame}
      */
     const clearAnimation = window.cancelAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-    /** canvas节点 */
-    const node = info.el;    
-    /** canvas 上下文 */
-    const context = info.el.getContext("2d");
+    const canvas = document.createElement("canvas");    
+    const context = canvas.getContext("2d");
     /** 结束角 */
     const CIRC = Math.PI * 2;
     /** 起始角，以弧度计（弧的圆形的三点钟位置是 0 度） */
     const QUART = Math.PI / 2;
+    /** 缩放比 */
+    const scaleValue = window.devicePixelRatio <= 1 ? 2 : window.devicePixelRatio;
     /** 当前动画的`id` */
     let animationId = 0;
     /** 圆环进度 0-1 */
     let progress = 0; 
     /** 当前进度 */
-    let progressNow = 50;
+    let progressNow = 68;
 
-    // 移动端上抗锯齿要乘以像素比，然后 canvas 设置 css { width: 100%; height: 100% }
-    node.width = (info.width || info.el.parentNode.clientWidth) * window.devicePixelRatio;
-    node.height = (info.height || info.el.parentNode.clientHeight) * window.devicePixelRatio;
+    canvas.style.width = "100%";
+    canvas.style.height = "100%";
+    info.el.appendChild(canvas);
 
+    // 移动端上抗锯齿要乘以像素比，然后 canvas 设置 css { width: 100%; height: 100% } ↑↑↑ 上面的代码
+    canvas.width = info.el.clientWidth * scaleValue;
+    canvas.height = info.el.clientHeight * scaleValue;
+    
     /** 圆环尺寸 */
-    const ringSize = Math.min(node.width, node.height);
+    const ringSize = Math.min(canvas.width, canvas.height);
     /** 中心坐标`x` */
-    const centerX = node.width / 2;
+    const centerX = canvas.width / 2;
     /** 中心坐标`y` */
-    const centerY = node.height / 2;
+    const centerY = canvas.height / 2;
 
     /** 绘制背景 */
     function drawBackground() {
@@ -61,11 +65,10 @@ function ringProgress(info) {
 
     /** 绘制底层圆环 */
     function drawLayerLine() {
-        // 初始化绘画圆环
         context.beginPath();
         context.strokeStyle = info.lineLayerColor || "#eeeeee";
         context.lineCap = "round";
-        context.lineWidth = info.lineWidth || 1;
+        context.lineWidth = (info.lineWidth || 1) * scaleValue;
         /** 圆环的半径 这里要减去圆环的线宽 */
         const radius = (ringSize - context.lineWidth) / 2; 
         context.arc(centerX, centerY, radius, -QUART, CIRC - QUART, false);
@@ -79,11 +82,10 @@ function ringProgress(info) {
      * @param {number} value
     */
     function drawLine(value) {
-        // 初始化绘画圆环
         context.beginPath();
         context.strokeStyle = info.lineColor || "orange";
         context.lineCap = "round";
-        context.lineWidth = info.lineWidth || 1;
+        context.lineWidth = (info.lineWidth || 1) * scaleValue;
         /** 圆环的半径 这里要减去圆环的线宽 */
         const radius = (ringSize - context.lineWidth) / 2; 
         context.arc(centerX, centerY, radius, -QUART, CIRC * value - QUART, false);
@@ -97,11 +99,13 @@ function ringProgress(info) {
      * @param {string} text
     */
     function drawText(text) {
+        // 不设置字体大小将不会显示字体
+        if (!info.fontSize) return; 
         // /** 画布上的矩形的像素数据 */
-        // const imgData = context.getImageData(0, 0, node.width, node.height);
+        // const imgData = context.getImageData(0, 0, canvas.width, canvas.height);
         // context.putImageData(imgData, 0, 0);
         // 设置文字样式
-        context.font = info.fontStyle || "bold 35px Arial";
+        context.font = `${ (info.fontWeight || "normal") } ${ (info.fontSize * scaleValue) }px ${ (info.fontStyle || "Arial") }`;
         context.textAlign = "center";
         context.textBaseline = "middle";
         context.fillStyle = info.fontColor || "orange";
@@ -122,7 +126,7 @@ function ringProgress(info) {
         }
 
         /** 百分比值 */
-        let value = progress / 100;
+        const value = progress / 100;
         drawBackground();
         drawLayerLine();
         drawLine(value);
@@ -150,15 +154,16 @@ function ringProgress(info) {
     }
 }
 
-const canvas = document.getElementById("ring");
+const ringBox = document.querySelector(".ring_box");
 
 const ring = ringProgress({
-    el: canvas,
-    width: 500,
-    height: 300,
-    lineWidth: 20,
+    el: ringBox,
+    lineWidth: 15,
     lineColor: "#6990f6",
-    fontColor: "#6990f6"
+    fontColor: "#6990f6",
+    fontSize: 24,
+    fontWeight: "bold",
+    fontStyle: "Helvetica"
 });
 
 /**
