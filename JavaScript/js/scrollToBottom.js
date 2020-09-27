ffca7dac93beb7ba8229cd0cc0438d15b9e851eb
1141752341@qq.com <z1141752341>
2020-09-27 19:22:50 +0800

    代码整理

--- JavaScript/js/scrollToBottom.js
+++ JavaScript/js/scrollToBottom.js
@@ -22,11 +22,13 @@
 
     /**
      * 监听滚动到底部（方法二）
-     * @param {() => void} callback 到达底部回调函数
-     * @param {number} number 距离底部多少像素触发（px）
-     * @param {boolean} once 是否为一次性（防止重复用）
+     * @param {object} options 传参对象
+     * @param {number} options.distance 距离底部多少像素触发（px）
+     * @param {boolean} options.once 是否为一次性（防止重复用）
+     * @param {() => void} options.callback 到达底部回调函数
      */
-    function scrollToBottom(callback, number = 0, once = false) {
+    function onScrollToBottom(options) {
+        const { distance = 0, once = false, callback = null } = options;
         const doc = document;
         /** 滚动事件 */
         function onScroll() {
@@ -34,7 +36,7 @@
             let scrollTop = doc.documentElement.scrollTop === 0 ? doc.body.scrollTop : doc.documentElement.scrollTop;
             /** 滚动条高度 */
             let scrollHeight = doc.documentElement.scrollTop === 0 ? doc.body.scrollHeight : doc.documentElement.scrollHeight;
-            if (scrollHeight - scrollTop - number <= window.innerHeight) {
+            if (scrollHeight - scrollTop - distance <= window.innerHeight) {
                 if (typeof callback === "function") callback();
                 if (once) window.removeEventListener("scroll", onScroll);
             }
@@ -44,37 +46,46 @@
         // onScroll(); 
     }
 
-    scrollToBottom(function () {
-        console.log("页面到底底部了，只监听一次");
-    }, 100, true);
+    onScrollToBottom({
+        distance: 100,
+        once: true,
+        callback() {
+            console.log("onScrollToBottom >> 滚动到底部 一次性");
+        }
+    });
 
     /**
      * 监听指定元素滚动到底部
-     * @param {HTMLElement} el 指定的元素
-     * @param {() => void} callback 到达底部回调函数
-     * @param {number} number 距离底部多少像素触发（px）
-     * @param {boolean} once 是否为一次性
+     * @param {object} options 传参对象
+     * @param {HTMLElement} options.el 指定的元素
+     * @param {number} options.distance 距离底部多少像素触发（px）
+     * @param {boolean} options.once 是否为一次性（防止重复用）
+     * @param {() => void} options.callback 到达底部回调函数
      */
-    function elementScrollToBottom(el, callback, number = 0, once = false) {
+    function onElementScrollToBottom(options) {
+        const { el, distance = 0, once = false, callback = null } = options;
         /** 滚动事件 */
         function onScroll() {
             /** 滚动的高度 */
             let scrollTop = el.scrollTop;
             /** 滚动条高度 */
             let scrollHeight = el.scrollHeight;
-            if (scrollHeight - scrollTop - number <= el.clientHeight) {
+            if (scrollHeight - scrollTop - distance <= el.clientHeight) {
                 if (typeof callback === "function") callback();
                 if (once) el.removeEventListener("scroll", onScroll);
             }
         }
         el.addEventListener("scroll", onScroll);
-        // 先执行一次
+        // 必要时先执行一次
         // onScroll();
     }
     const scrollEl = document.querySelector(".scroll_view");
 
-    elementScrollToBottom(scrollEl, function () {
-        console.log("节点滚动到底部了");
+    onElementScrollToBottom({
+        el: scrollEl,
+        callback() {
+            console.log("节点滚动到底部了");
+        }
     });
 
 })();
