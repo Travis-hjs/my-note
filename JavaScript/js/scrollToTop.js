b60d48dfa190723adf7f4b75642c9adf2b733c0c
黄景圣 <1141752341@qq.com>
2021-07-15 11:30:14 +0800

    返回顶部代码优化

--- JavaScript/js/scrollToTop.js
+++ JavaScript/js/scrollToTop.js
@@ -3,12 +3,16 @@
      * 缓动滚动到顶部
      * @description 兼容移动端  && IE-10+
      * @param {HTMLElement} el 
+     * @param {number} distance 距离多少显示当前元素，默认滚动条的 1/3
      */
-    function scrollToTop(el) {
+    function scrollToTop(el, distance = null) {
+        const body = document.body;
         /**
+         * 动画函数
          * @type {requestAnimationFrame}
          */
         const animation = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+        /** 节点原来的属性值 */
         const beforeDisplay = el.style.display || document.defaultView.getComputedStyle(el).display;
         /**
          * @type {HTMLElement}
@@ -29,29 +33,50 @@
         }
 
         function onScroll() {
-            rootNode = document.body.scrollTop === 0 ? document.documentElement : document.body;
-            if (rootNode.scrollTop > (document.body.scrollHeight / 2)) {
+            rootNode = body.scrollTop === 0 ? document.documentElement : body;
+            if (rootNode.scrollTop > (distance || ((body.scrollHeight / 3)))) {
                 el.style.display = beforeDisplay;
             } else {
                 el.style.display = "none";
             }
         }
-        // 先执行一次
-        onScroll();
 
-        window.addEventListener("scroll", onScroll);
-        // 滚轮, 触摸事件
-        // Firefox下要用 DOMMouseScroll 代替 mousewheel
-        document.body.addEventListener("DOMMouseScroll", e => isMove = false);
-        document.body.addEventListener("mousewheel", e => isMove = false);
-        document.body.addEventListener("touchmove", e => isMove = false);
-
-        el.addEventListener("click", e => {
+        function main() {
             if (isMove) return;
             isMove = true;
             move();
-        });
+        }
+
+        function setState() {
+            isMove = false;
+        }
+        
+        /** 移除所有监听事件 */
+        function removeEvent() {
+            body.removeEventListener("DOMMouseScroll", setState);
+            body.removeEventListener("mousewheel", setState);
+            body.removeEventListener("touchmove", setState);
+            window.removeEventListener("scroll", onScroll);
+            el.removeEventListener("click", main);
+        }
+
+        // 先执行一次
+        onScroll();
+        // 先移除，再监听，防止多次添加事件
+        removeEvent();
+
+        // Firefox下要用 DOMMouseScroll 代替 mousewheel
+        body.addEventListener("DOMMouseScroll", setState);
+        body.addEventListener("mousewheel", setState);
+        body.addEventListener("touchmove", setState);
+        window.addEventListener("scroll", onScroll);
+        el.addEventListener("click", main);
+
+        return {
+            removeEvent
+        }
     }
+
     scrollToTop(document.querySelector(".goback"));
 
 
