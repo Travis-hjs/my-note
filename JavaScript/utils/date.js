994c924357b3be46359430765439dd2c5dba2f5f
黄景圣 <1141752341@qq.com>
2022-07-29 16:39:03 +0800

    日期工具修改

--- JavaScript/utils/date.js
+++ JavaScript/utils/date.js
@@ -6,61 +6,6 @@
 // new Date().toLocaleTimeString();         => "上/下午12:12:12"
 // new Date().toLocaleString();             => "2020/12/12 上/下午12:12:12"
 
-/** 日期列表生成 */
-function dateJson() {
-  /**
-   * 日历数组
-   * @type {Array<{label: string, children: Array<{label: string, children: Array<{label: string}>}>}>}
-   */
-  const calendar = [];
-  const date = new Date();
-  const minYear = date.getFullYear();
-  const maxYear = date.getFullYear() + 10;
-  let dayCount = 1;
-  for (let i = minYear; i <= maxYear; i++) {
-    const year = {
-      label: i.toString(),
-      children: []
-    }
-    for (let j = 1; j <= 12; j++) {
-      const month = {
-        label: ("0" + j.toString()).slice(-2),
-        children: []
-      }
-      year.children.push(month);
-      dayCount = new Date(i, j, 0).getDate();
-      for (let k = 1; k <= dayCount; k++) {
-        month.children.push({
-          label: ("0" + k.toString()).slice(-2)
-        });
-      }
-    }
-    calendar.push(year);
-  }
-  // 这里是限制不能选小于之前的日期
-  calendar[0].children.splice(0, date.getMonth());
-  calendar[0].children[0].children.splice(0, date.getDate());
-  return calendar;
-}
-
-/**
- * 时间生成器
- * @param {number} minInterval 时间间隔(分钟)
- */
-function timeInterval(minInterval) {
-  let arr = [];
-  let minTotal = 0;
-  while (minTotal < 1440) {
-    let hour = Math.floor(minTotal / 60);
-    let minute = Math.floor(minTotal % 60);
-    hour = ("0" + hour).slice(-2);
-    minute = ("0" + minute).slice(-2);
-    minTotal += minInterval;
-    arr.push(hour + ":" + minute);
-    return arr;
-  }
-}
-
 /**
  * 格式化日期
  * @param {string | number | Date} value 指定日期
@@ -104,22 +49,36 @@ function getDateDayString(value) {
 }
 
 /**
- * 获取两个时间段的秒数
- * @param {Date} now 现在时间
- * @param {Date} before 之前的时间
+ * 获取某年某月有多少天
+ * @param {number} year 指定年份
+ * @param {number} month 指定月份
+ * @returns 
  */
-function getDateSlotSecond(now, before) {
-  return (now.getTime() - before.getTime()) / 1000;
+function getMonthDay(year, month) {
+  const day31 = [1, 3, 5, 7, 8, 10, 12];
+  const day30 = [4, 6, 9, 11];
+  if (day31.includes(month)) {
+    return 31;
+  } else if (day30.includes(month)) {
+    return 30;
+  } else {
+    if ((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)) {
+      return 29;
+    } else {
+      return 28;
+    }
+  }
 }
 
 /**
  * 获取两个日期之间的天数
- * @param {Date|string|number} date1 
- * @param {Date|string|number} date2 
+ * @param {Date | string | number} date1 
+ * @param {Date | string | number} date2 
  * @returns 
  */
 function daysBetween(date1, date2) {
-  return Math.ceil(Math.abs(new Date(date1) - new Date(date2)) / (1000 * 60 * 60 * 24));
+  const value = new Date(date1).getTime() - new Date(date2).getTime();
+  return Math.ceil(Math.abs(value) / (1000 * 60 * 60 * 24));
 }
 
 /**
@@ -142,4 +101,59 @@ function formatSecond(value, withDay = false) {
   minute = ("0" + minute).slice(-2);
   second = ("0" + second).slice(-2);
   return { day, hour, minute, second }
+}
+
+/** 日期列表生成 */
+function dateJson() {
+  /**
+   * 日历数组
+   * @type {Array<{label: string, children: Array<{label: string, children: Array<{label: string}>}>}>}
+   */
+  const calendar = [];
+  const date = new Date();
+  const minYear = date.getFullYear();
+  const maxYear = date.getFullYear() + 10;
+  let dayCount = 1;
+  for (let i = minYear; i <= maxYear; i++) {
+    const year = {
+      label: i.toString(),
+      children: []
+    }
+    for (let j = 1; j <= 12; j++) {
+      const month = {
+        label: ("0" + j.toString()).slice(-2),
+        children: []
+      }
+      year.children.push(month);
+      dayCount = new Date(i, j, 0).getDate();
+      for (let k = 1; k <= dayCount; k++) {
+        month.children.push({
+          label: ("0" + k.toString()).slice(-2)
+        });
+      }
+    }
+    calendar.push(year);
+  }
+  // 限制不能选小于之前的日期
+  calendar[0].children.splice(0, date.getMonth());
+  calendar[0].children[0].children.splice(0, date.getDate());
+  return calendar;
+}
+
+/**
+ * 时间生成器
+ * @param {number} minInterval 时间间隔(分钟)
+ */
+function timeInterval(minInterval) {
+  let arr = [];
+  let minTotal = 0;
+  while (minTotal < 1440) {
+    let hour = Math.floor(minTotal / 60);
+    let minute = Math.floor(minTotal % 60);
+    hour = ("0" + hour).slice(-2);
+    minute = ("0" + minute).slice(-2);
+    minTotal += minInterval;
+    arr.push(hour + ":" + minute);
+    return arr;
+  }
 }
