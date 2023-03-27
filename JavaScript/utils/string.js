decf100a22782a0c5522633a6c1c6393ce2dee9a
黄景圣 <1141752341@qq.com>
2023-03-27 12:55:16 +0800

    字符串substr方法替换为自定义方法

--- JavaScript/utils/string.js
+++ JavaScript/utils/string.js
@@ -198,6 +198,27 @@ function getMarkHtml(content, keywords) {
 }
 
 /**
+ * 实现原生废弃的`String.prototype.substr()`方法
+ * @param {string} value 
+ * @param {number} start 
+ * @param {number} length 
+ */
+function substr(value, start = 0, length = value.length) {
+  if (length < 0) return "";
+  const _length = value.length;
+  if (start <= -_length) {
+    start = 0;
+  }
+  start = start < 0 ? _length + start : start;
+  length = start + length > _length ? _length : start + length;
+  let result = "";
+  for (let i = start; i < length; i++) {
+    result += value[i];
+  }
+  return result;
+}
+
+/**
  * 数字转中文
  * @param {number} target 
  */
@@ -210,8 +231,8 @@ function numberToChinese(target) {
   const cnInteger = "整";
   const cnIntLast = "元";
   const maxNum = 999999999999999.9999;
-  let IntegerNum = "";
-  let DecimalNum = "";
+  let integerNum = "";
+  let decimalNum = "";
   let result = "";
   /** @type {Array<string>} */
   let parts;
@@ -225,18 +246,18 @@ function numberToChinese(target) {
   }
   const numStr = target.toString();
   if (numStr.indexOf(".") == -1) {
-    IntegerNum = numStr;
+    integerNum = numStr;
   } else {
     parts = numStr.split(".");
-    IntegerNum = parts[0];
-    DecimalNum = parts[1].substr(0, 4);
+    integerNum = parts[0];
+    decimalNum = substr(parts[1], 0, 4);
   }
   // 获取整型部分转换
-  if (parseInt(IntegerNum, 10) > 0) {
+  if (parseInt(integerNum, 10) > 0) {
     let zeroCount = 0;
-    const IntLen = IntegerNum.length;
+    const IntLen = integerNum.length;
     for (let i = 0; i < IntLen; i++) {
-      const n = IntegerNum.substr(i, 1);
+      const n = substr(integerNum, i, 1);
       const p = IntLen - i - 1;
       const q = p / 4;
       const m = p % 4;
@@ -256,10 +277,10 @@ function numberToChinese(target) {
     result += cnIntLast;
   }
   // 小数部分
-  if (DecimalNum != "") {
-    const decLen = DecimalNum.length;
+  if (decimalNum != "") {
+    const decLen = decimalNum.length;
     for (let i = 0; i < decLen; i++) {
-      const n = DecimalNum.substr(i, 1);
+      const n = substr(decimalNum, i, 1);
       if (n != "0") {
         result += cnNums[Number(n)] + cnDecUnits[i];
       }
@@ -267,7 +288,7 @@ function numberToChinese(target) {
   }
   if (result == "") {
     result += cnNums[0] + cnIntLast + cnInteger;
-  } else if (DecimalNum == "") {
+  } else if (decimalNum == "") {
     result += cnInteger;
   }
   return result;
