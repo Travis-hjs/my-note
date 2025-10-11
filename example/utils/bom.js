03760aea77a6cbe510b89c9e308b45ee0dfeae69
Travis-黄景圣 <1141752341@qq.com>
2025-10-11 10:21:41 +0800

    perf: 获取cookie方法优化

--- example/utils/bom.js
+++ example/utils/bom.js
@@ -246,7 +246,9 @@ function downloadFile(blob, filename) {
 function getCookie(key) {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${key}=`);
-  if (parts.length === 2) return parts.pop().split(";").shift();
+  if (parts.length >= 2) {
+    return parts.pop().split(";").shift();
+  }
   return "";
 }
 
